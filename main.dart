89a22488923dba3d51c2451175ad16850b1a4d54
Rwema <52289151+rwema3@users.noreply.github.com>
2022-03-19 20:47:26 +0200

    Update main.dart

--- main.dart
+++ main.dart
@@ -18,3 +18,4 @@ void main() async {
   await SimplePermissions.requestPermission(Permission.ReadExternalStorage);
   PermissionStatus permission = await SimplePermissions.requestPermission(Permission.WriteExternalStorage);
   print(permission);
+  setupLocator();
