bfd9fd295b8f60858b0e959954261e91782dec7c
Rwema <52289151+rwema3@users.noreply.github.com>
2022-03-19 20:49:52 +0200

    Update main.dart

--- main.dart
+++ main.dart
@@ -63,3 +63,4 @@ class Wrapper extends StatelessWidget {
       panel: NowPlayingScreen(controller: layoutService.albumPlayerPageController),
       controller: layoutService.globalPanelController,
       minHeight: 60,
+      maxHeight: MediaQuery.of(context).size.height,
