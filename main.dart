26ac38790ce0983ea010d553d361bef44cbd06d7
Rwema <52289151+rwema3@users.noreply.github.com>
2022-03-20 17:08:51 +0200

    Update main.dart

--- main.dart
+++ main.dart
@@ -81,4 +81,3 @@ class Wrapper extends StatelessWidget {
       body: MaterialApp(
         debugShowCheckedModeBanner: false,
         title: "Tune In Music Player",
-        color: MyTheme.darkRed,
