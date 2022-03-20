d8f4079f0d2651ef3cf1e665c15ffd02d1b337dd
Rwema <52289151+rwema3@users.noreply.github.com>
2022-03-20 19:16:13 +0200

    Update main.dart

--- main.dart
+++ main.dart
@@ -16,3 +16,4 @@ Nano nano = Nano();
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await SimplePermissions.requestPermission(Permission.ReadExternalStorage);
+  PermissionStatus permission = await SimplePermissions.requestPermission(Permission.WriteExternalStorage);
