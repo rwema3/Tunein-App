d1411627fac39415172c42a4b1abd9be78a670f2
Rwema <52289151+rwema3@users.noreply.github.com>
2022-03-20 18:59:47 +0200

    Update main.dart

--- main.dart
+++ main.dart
@@ -4,4 +4,4 @@ import 'package:Tunein/globals.dart';
 import 'package:Tunein/plugins/nano.dart';
 import 'package:Tunein/root.dart';
 import 'package:Tunein/services/layout.dart';
-import 'package:Tunein/services/locator.dart'
+import 'package:Tunein/services/locator.dart';
