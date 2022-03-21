cb0fd15bb0d333b96d80670bcc83a065558c06b2
Rwema <52289151+rwema3@users.noreply.github.com>
2022-03-21 15:34:20 +0200

    Update TuneMotor.dart

--- TuneMotor.dart
+++ TuneMotor.dart
@@ -15,4 +15,4 @@ Nano nano = Nano();
 void main() async {
     WidgetsFlutterBinding.ensureInitialized();
     await SimplePermissions.requestPermission(Permission.ReadExternalStorage);
-    PermissionStatus permission = await SimplePer
+    PermissionStatus permission = await SimplePermi
