7018dc1fbd5515dcacd19ca93c1a5b1265108c58
Rwema <52289151+rwema3@users.noreply.github.com>
2022-03-21 14:01:13 +0200

    Update TuneMotor.dart

--- TuneMotor.dart
+++ TuneMotor.dart
@@ -11,4 +11,4 @@ import 'package:simple_permissions/simple_permissions.dart';
 import 'package:sliding_up_panel/sliding_up_panel.dart';
 import 'services/locator.dart';
 import 'services/languageService.dart';
-Nano nano = Nano
+Nano nano = Nan
