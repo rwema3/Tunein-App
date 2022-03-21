fc439f0e2048906b807475a0579a443923a041b9
Rwema <52289151+rwema3@users.noreply.github.com>
2022-03-21 15:48:46 +0200

    Update TuneMotor.dart

--- TuneMotor.dart
+++ TuneMotor.dart
@@ -70,3 +70,4 @@ class Wrapper extends StatelessWidget {
                 },
                 onPanelSlide: (value){
                     if(value>=0.3){
+                        layoutService.onPanelOpen(value);
