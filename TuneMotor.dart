ac352ffbd4940469779221521aa104e660eaab22
Rwema <52289151+rwema3@users.noreply.github.com>
2022-03-21 15:31:16 +0200

    Update TuneMotor.dart

--- TuneMotor.dart
+++ TuneMotor.dart
@@ -14,4 +14,4 @@ import 'services/languageService.dart';
 Nano nano = Nano();
 void main() async {
     WidgetsFlutterBinding.ensureInitialized();
-    await SimplePermissions.requestPermission(Permission.ReadExternalStora
+    await SimplePermissions.requestPermission(Permission.ReadExternalStorag
