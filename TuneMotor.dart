0f5659404e56b29eeb084e3ba597726b48bbd2bf
Rwema <52289151+rwema3@users.noreply.github.com>
2022-03-21 13:44:23 +0200

    Update TuneMotor.dart

--- TuneMotor.dart
+++ TuneMotor.dart
@@ -4,3 +4,4 @@ import 'package:Tunein/plugins/nano.dart';
 import 'package:Tunein/root.dart';
 import 'package:Tunein/globals.dart';
 import 'package:Tunein/services/layout.dart';
+import 'package:Tunein/services/locator.dart';
