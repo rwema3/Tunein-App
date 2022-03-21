ba8df79c00ea358700025c1c490fa97c478f3369
Rwema <52289151+rwema3@users.noreply.github.com>
2022-03-21 15:03:19 +0200

    Update TuneMotor.dart

--- TuneMotor.dart
+++ TuneMotor.dart
@@ -1,2 +1,5 @@
 import 'package:Tunein/components/bottomPanel.dart';
-import 'package:Tunein/components/
+import 'package:Tunein/components/playing.dart';
+import 'package:Tunein/plugins/nano.dart';
+import 'package:Tunein/root.dart';
+import 'package:Tunein/globals.dart';
